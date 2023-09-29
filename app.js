056bbabe077ca4d199bc01513135d1fb42ab26e2
dianaveselkina <vess_di@mail.ru>
2023-09-29 21:31:23 +0300

    add slider

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ function updateCounter() {
   counter.innerHTML = `${hour}: ${min}: ${sec}`;
   time--;
 }
-document.body.addEventListener('click', (e) => {
-  if (!e.target.matches('button')) return;
-  document.querySelector('.pic img').src = e.target.dataset.src;
-
-  document
-    .querySelectorAll('button')
-    .forEach((btn) => btn.classList.remove('active'));
-  e.target.classList.add('active');
-});
+function imgUrl(image) {
+  document.getElementById('img-slider').src = image;
+}
